6208a26004b44c52f72c3e176da70932d1725ba9
igormivanov <igormivanovx@gmail.com>
2024-02-18 19:17:34 -0300

    Initial commit

---
+++ src/main/java/br/com/igorivanov/sdi/SdiApplication.java
@@ -0,0 +1,13 @@
+package br.com.igorivanov.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiApplication.class, args);
+	}
+
+}
